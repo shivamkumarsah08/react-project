56070cd453f710a443154fdd156d18ca2c2337f9
Shivam <shivamawgp111@gmail.com>
2024-04-07 15:57:42 +0530

    update1

--- src/containers/Users.js
+++ src/containers/Users.js
@@ -1,32 +1,27 @@
-import React, { useState ,useEffect} from "react"
+import React,{useState,useEffect} from "react";
 function Users(){
-    const[users,setUsers]=useState([
-        {name:'mohan',age:23},
-        {name:'sohan',age:26}
+    const [users,setUsers] = useState([
+         {name:'mohan',age:23},
+         {
+             name:'sohan',age:24
+         }
     ]);
-    useEffect(()=>
-    {
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response=>response.json())
-        .then(data=>{
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json()).then(data =>{
             setUsers(data)
         })
-    }
-)
-    
-}
-function User(){
-    return(
-        <div>
-            <h2> Users</h2>
-            <ul>
-                {
-                    Users.map((user,index)=>{
-                        return <li>{user.name},{user.email}</li>
-                    })
-                }
-            </ul>
-        </div>
+    },[])
+    return (
+    <div>
+        <h2>Users</h2>
+        <ul>
+            {
+                users.map((user,index) =>{
+                    return <li>{user.name},{user.email}</li>
+                })
+            }
+        </ul>
+    </div>
     )
 }
 export default Users;
